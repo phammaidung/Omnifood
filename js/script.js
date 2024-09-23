264cb94bb6ac857f0f4c80769edb1b8251e0aef8
phammaidung <phammaidung8@gmail.com>
2024-09-23 18:18:50 +0700

    implement a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const yearEl = document.querySelector('.year');
 const curYear = new Date().getFullYear();
 yearEl.textContent = curYear;
 
+///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
 const headerEl = document.querySelector('.header');
@@ -14,6 +15,7 @@ toggleBtnEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
+///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 const allLinks = document.querySelectorAll('a:link');
 
@@ -40,6 +42,7 @@ allLinks.forEach(function (link) {
   });
 });
 
+///////////////////////////////////////////////////////////
 // Visible button back to top
 
 const btnUpToTop = document.querySelector('.btn-up-nav');
@@ -61,6 +64,30 @@ function visibleBtnBackTop() {
 }
 visibleBtnBackTop();
 
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+const obs = new IntersectionObserver(
+  function (entries) {
+    const entry = entries[0];
+    if (!entry.isIntersecting) {
+      document.body.classList.add('sticky');
+    }
+    if(entry.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement('div');
@@ -80,55 +107,5 @@ function checkFlexGap() {
 }
 checkFlexGap();
 
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
 
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
